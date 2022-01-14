297309deb5283c300c35fd3ac67292412d971900
wiktor <wiktor.nizio@gmail.com>
2022-01-14 04:37:51 +0100

    Added animations.

--- licznik/src/main/java/pl/org/seva/licznik/main/MainFragment.kt
+++ licznik/src/main/java/pl/org/seva/licznik/main/MainFragment.kt
@@ -1,10 +1,18 @@
 package pl.org.seva.licznik.main
 
-import android.graphics.PorterDuff
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.skip
+import kotlinx.coroutines.launch
 import pl.org.seva.licznik.R
 import pl.org.seva.licznik.databinding.FrMainBinding
 import pl.org.seva.licznik.main.extension.invoke
@@ -22,8 +30,6 @@ class MainFragment : Fragment(R.layout.fr_main) {
     ): View {
         setHasOptionsMenu(true)
         binding = FrMainBinding.inflate(inflater, container, false)
-        binding.lifecycleOwner = this
-        binding.vm = vm
         return binding.root
     }
 
@@ -31,6 +37,32 @@ class MainFragment : Fragment(R.layout.fr_main) {
         setHasOptionsMenu(true)
         binding.button {
             vm.counter.value++
+        }
+        binding.number.text = vm.counter.value.toString()
+
+        lifecycleScope.launch {
+            vm.counter
+                .flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+                .collect { value ->
+                    val vanish = if (value.mod(2) == 0) binding.number else binding.numberOdd
+                    val appear = if (value.mod(2) == 0) binding.numberOdd else binding.number
+
+                    vanish.animate().alpha(0.0F)
+                        .setListener(object : AnimatorListenerAdapter() {
+                            override fun onAnimationEnd(animation: Animator?) {
+                                vanish.visibility = View.INVISIBLE
+                            }
+                        })
+
+                    appear.text = value.toString()
+                    appear.visibility = View.VISIBLE
+                    appear.animate().alpha(1.0F)
+                        .setListener(object : AnimatorListenerAdapter() {
+                            override fun onAnimationEnd(animation: Animator?) {
+                                appear.visibility = View.VISIBLE
+                            }
+                        })
+                }
         }
     }
 
