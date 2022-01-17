2924e52eb81344aa11cffdea3a790ec016675ae9
wiktor <wiktor.nizio@gmail.com>
2022-01-17 18:42:38 +0100

    Removed a redundant line.

--- licznik/src/main/java/pl/org/seva/licznik/main/CounterApplication.kt
+++ licznik/src/main/java/pl/org/seva/licznik/main/CounterApplication.kt
@@ -1,7 +1,6 @@
 package pl.org.seva.licznik.main
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.color.DynamicColors
 
 @Suppress("unused")
@@ -9,7 +8,6 @@ class CounterApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         DynamicColors.applyToActivitiesIfAvailable(this)
     }
 }
